2bb9f882a06724689e5f20c5257255fb6cf35061
oxeone <bb.denmark@gmail.com>
2025-04-03 10:35:17 +0200

    pcats

--- api/models/Categories.js
+++ api/models/Categories.js
@@ -2,60 +2,32 @@ const { logger } = require('~/config');
 
 const options = [
   {
-    label: 'com_ui_idea',
-    value: 'idea',
+    label: 'com_ui_product_marketing',
+    value: 'product_marketing',
   },
   {
-    label: 'com_ui_travel',
-    value: 'travel',
+    label: 'com_ui_marketing_analytics',
+    value: 'marketing_analytics',
   },
   {
-    label: 'com_ui_teach_or_explain',
-    value: 'teach_or_explain',
-  },
-  {
-    label: 'com_ui_write',
-    value: 'write',
-  },
-  {
-    label: 'com_ui_shop',
-    value: 'shop',
-  },
-  {
-    label: 'com_ui_code',
-    value: 'code',
-  },
-  {
-    label: 'com_ui_misc',
-    value: 'misc',
-  },
-  {
-    label: 'com_ui_roleplay',
-    value: 'roleplay',
-  },
-  {
-    label: 'com_ui_finance',
-    value: 'finance',
+    label: 'com_ui_content_strategy',
+    value: 'content_strategy',
   },
   {
     label: 'com_ui_branding',
     value: 'branding',
   },
   {
-    label: 'com_ui_copywriting',
-    value: 'copywriting',
+    label: 'com_ui_social_media',
+    value: 'social_media',
   },
   {
     label: 'com_ui_email_marketing',
     value: 'email_marketing',
   },
   {
-    label: 'com_ui_social_media',
-    value: 'social_media',
-  },
-  {
-    label: 'com_ui_content_strategy',
-    value: 'content_strategy',
+    label: 'com_ui_copywriting',
+    value: 'copywriting',
   },
   {
     label: 'com_ui_seo',
@@ -70,12 +42,12 @@ const options = [
     value: 'customer_research',
   },
   {
-    label: 'com_ui_product_marketing',
-    value: 'product_marketing',
+    label: 'com_ui_code',
+    value: 'code',
   },
   {
-    label: 'com_ui_marketing_analytics',
-    value: 'marketing_analytics',
+    label: 'com_ui_misc',
+    value: 'misc',
   },
 ];
 
