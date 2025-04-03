183d5a0e400cbfa0583082b2de696d8522a9f481
oxeone <bb.denmark@gmail.com>
2025-04-03 22:18:24 +0200

    rename

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       },
       includeAssets: ['**/*'],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'MChat',
+        short_name: 'MChat',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
